325a735db64433c99a20a4e1b246f6d192a19890
Marco Zink <marcozink@gmail.com>
2016-05-18 18:38:00 -0500

    Update click2call.php

--- click2call.php
+++ click2call.php
@@ -1,51 +1,29 @@
 <?php
-// *** Parámetros de configuración
-// * Asterisk Manager
-define('HOST','curadeuda.no-ip.org:35000');
-define('USER','c2c');
-define('PASS','ast3r!sk');
-
-// * Formato de click2call
-define('PREFIX','C2C ');					// Prefijo a anteponer al nombre del cliente
-
-// * Plan de marcación
-define('CONTEXT','from-internal');			// Contexto donde el numero XXXXXXXXXX debe ser permitido
-define('AGENT','local/912@from-internal');					// Canal del/los agentes que recibiran la llamada
-define('TIMEOUT',25000);					// Tiempo máximo para que el agente conteste
-
-// * Redireccion en caso de exito
-define('REDIRECT_SUCCESS','http://enlaza.mx/contacto+exitoso');
-// * Redireccion en caso de falla
-define('REDIRECT_FAILURE','http://enlaza.mx/contacto+error');
-
-
-
-
-
-/* NO CAMBIA NADA A PARTIR DE ESTE PUNTO */
-
-// Si no hay variable submit, entonces es la primera vez que llegamos y no hacemos nada
-if (!isset($_POST['numero']) || !is_numeric($_POST['numero']) || !isset($_POST['nombre']))
-	die("Alguno de los números no fue proporcionado o no está en formato numérico.");
-
-// Todo está bien. Hay que hacer magia	
-
+define('HOST','server');
+define('USER','amiuser');
+define('PASS','amipass');
+define('PREFIX','wp-c2c ');
+define('CONTEXT','context');
+define('AGENT','agemt');
+define('TIMEOUT',25000);
+define('REDIRECT_SUCCESS','urlsuccess');
+define('REDIRECT_FAILURE','urlfail');
+if (!isset($_POST['number']) || !is_numeric($_POST['number']) || !isset($_POST['name']))
+	die("Incorrect input data");
 require_once "phpagi-asmanager.php";
 $agi = new AGI_AsteriskManager();
 if (!$agi->Connect(HOST,USER,PASS))
-	die("No es posible conectar con el marcador. Revise su configuración o su conexión a la red.");
-
-// Solicitamos la llamada
-$exito = $agi->Originate(
-	AGENT,												// Channel
-	null,												// Exten
-	null,												// Context
-	null,												// Priority
+	die("No connection to host, check data.");
+$success = $agi->Originate(
+	AGENT,
+	null,
+	null,
+	null,
 	'Dial',
-	sprintf("Local/%s@%s",$_POST['numero'],CONTEXT),
-	TIMEOUT,											// Timeout
-	sprintf("%s%s <%s>",PREFIX,$_POST['nombre'],$_POST['numero'])// CallerID a desplegar al agente
+	sprintf("Local/%s@%s",$_POST['number'],CONTEXT),
+	TIMEOUT,
+	sprintf("%s%s <%s>",PREFIX,$_POST['name'],$_POST['number'])
 );
-header('Location: '.(($exito)?REDIRECT_SUCCESS:REDIRECT_FAILURE));
+header('Location: '.(($success)?REDIRECT_SUCCESS:REDIRECT_FAILURE));
 die();
 ?>
